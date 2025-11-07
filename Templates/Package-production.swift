634d8c82e61990016b244ccf5fd676a3e28f43b1
Tony Arnold <tony@thecocoabots.com>
2025-11-08 08:21:04 +1100

    Fix busted Package.swift
    Fixes #26

--- Templates/Package-production.swift
+++ Templates/Package-production.swift
@@ -24,11 +24,16 @@ let package = Package(
         .plugin(name: "DependencyAuditPlugin", targets: ["DependencyAuditPlugin"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.6.1")
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.6.1"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "601.0.1"),
     ],
     targets: [
         .target(
             name: "SwiftDependencyAuditLib",
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftParser", package: "swift-syntax"),
+            ],
             plugins: ["VersionPlugin"]
         ),
         .executableTarget(
@@ -46,7 +51,8 @@ let package = Package(
         ),
         .testTarget(
             name: "SwiftDependencyAuditTests",
-            dependencies: ["SwiftDependencyAuditLib"]
+            dependencies: ["SwiftDependencyAuditLib"],
+            resources: [.copy("Fixtures")]
         ),
         .plugin(
             name: "DependencyAuditPlugin",
